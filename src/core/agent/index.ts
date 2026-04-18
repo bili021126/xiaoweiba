e7ab9a3b2989b024dbb88666bc2282d569231e58
liuyuan <1770643458@qq.com>
2026-04-18 17:27:28 +0800

    chore: clean main branch for planning purposes

--- src/core/agent/index.ts
+++
@@ -1,3 +0,0 @@
-export { IAgent, AgentCapability, AgentMetadata, AgentResult, AgentInput, AgentFactory, AgentRegistration } from './IAgent';
-export { AgentManager } from './AgentManager';
-export { ChatAgent } from './ChatAgent';
